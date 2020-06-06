3d8136d04e75fcb02de7cc496df0f5085a6eac21
Yulin Xue <yulinxue@Yulins-MacBook-Pro.local>
2020-06-06 16:29:03 -0400

    Basic UI Setup

--- src/app/CreateEvent/tab2.page.ts
+++ src/app/CreateEvent/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public toastController: ToastController) {}
 
+  titleText: string;
+  locationText: string;
+  startDateText = '2020-05-25';
+  startTimeText: string;
+  endTimeText: string;
+  endDateText: string;
+
+  async createEvent(){
+    const toast = await this.toastController.create({
+      header: 'Event has been created successfully',
+      message: 'Click to Close',
+      position: 'top',
+      duration: 2000,
+      buttons: [
+        {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
 }
